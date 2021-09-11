01e2c6443a81f3a8d881e82d876d57cf58fe43eb
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-09-11 09:09:24 +0530

    Remove

--- Apps/dotfiles/.config/BraveSoftware/Brave-Browser/Default/Extensions/gjdldigdojpjlmphnogmcmhojfadfmem/1.5_0/background.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-let disableShelf = () => chrome.downloads.setShelfEnabled(false);
-chrome.runtime.onInstalled.addListener(disableShelf);
-chrome.runtime.onStartup.addListener(disableShelf);
