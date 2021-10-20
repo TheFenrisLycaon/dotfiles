81fd5e5d2e632acadbefb1fa24538e94bdfc60f4
Rishabh Anand <rishabhanandxz@gmail.com>
2021-10-20 12:44:57 +0530

    Auto-Update
    This commit was made automatically

--- Apps/dotfiles/autoUpdate.sh
+++ Apps/dotfiles/autoUpdate.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd /home/user
+cd /home/fenris
 /usr/bin/git --git-dir=/home/fenris/.cfg/ --work-tree=/home/fenris add -u
 /usr/bin/git --git-dir=/home/fenris/.cfg/ --work-tree=/home/fenris commit -m "Auto-Update" -m "This commit was made automatically"
 /usr/bin/git --git-dir=/home/fenris/.cfg/ --work-tree=/home/fenris push
